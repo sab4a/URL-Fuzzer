fd158fd1c70c9a641e6e8dfc4745b86f527d6e5e
Saba Meskhi <36389381+sab4a@users.noreply.github.com>
2018-05-17 01:33:29 +0400

    Add files via upload

---
+++ WPChecker/WPcheck.py
@@ -0,0 +1,26 @@
+﻿import requests
+
+Website_Wordlist = open('parsed.txt','r')
+
+for line in Website_Wordlist:
+
+    try:
+        page = requests.get('http://' + line.replace('\n','') + '/license.txt')
+        txt = page.text
+
+        if 'WordPress' in txt:
+            print( '\n\033[1;32m {}is powered by wordpress \033[1;m'.format(line) )
+
+            outF = open("wp.txt", "a")
+            outF.write(line)
+            outF.close()
+        else:
+            print( "\n{}is not powered by wordpress ".format(line) )
+
+    except:
+
+        print( "\n" + line + "Website is down")
+        continue
+
+
+
